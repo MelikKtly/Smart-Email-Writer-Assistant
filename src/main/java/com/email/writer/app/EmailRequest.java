3d10b5cf946f4df8a44a2bdaee8d2aa095cf0df7
MelikKtly <melikatilla@hotmail.com>
2025-09-08 02:13:06 +0300

    .

---
+++ src/main/java/com/email/writer/app/EmailRequest.java
@@ -0,0 +1,15 @@
+package com.email.writer.app;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailRequest {
+    private String emailContent;
+    private String tone;
+}
