58cd88efaeddd303e75980f60518ddfdf4dd0b7d
MelikKtly <melikatilla@hotmail.com>
2025-09-08 04:02:34 +0300

    Backend is done

--- src/main/java/com/email/writer/app/EmailGeneratorService.java
+++ src/main/java/com/email/writer/app/EmailGeneratorService.java
@@ -1,5 +1,7 @@
 package com.email.writer.app;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -18,8 +20,8 @@ public class EmailGeneratorService {
     @Value("${gemini.api.key}")
     private String geminiApiKey;
 
-    public EmailGeneratorService(WebClient webClient) {
-        this.webClient = webClient;
+    public EmailGeneratorService(WebClient.Builder webClient) {
+        this.webClient = WebClient.builder().build();
     }
 
 
@@ -37,12 +39,29 @@ public class EmailGeneratorService {
             String response= webClient.post()
                     .uri(geminiApiUrl+geminiApiKey)
                     .header("Content-Type","application/json")
+                    .bodyValue(requestBody)
                     .retrieve()
                     .bodyToMono(String.class)
                     .block();
 
-            // Return response
+            // Extract Response and Return
+            return extractResponseContent(response);
+    }
 
+    private String extractResponseContent(String response) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(response);
+            return rootNode.path("candidates")
+                    .get(0)
+                    .path("content")
+                    .path("parts")
+                    .get(0)
+                    .path("text")
+                    .asText();
+        }catch (Exception e){
+            return "Error processing request: "+ e.getMessage();
+        }
     }
 
     private String buildPrompt(EmailRequest emailRequest) {
